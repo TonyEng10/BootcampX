5fe46fcf72d25e8a4451769b4c22c9f4b823826c
Tony Eng <tony.eng@telus.com>
2022-12-12 17:00:07 -0800

    bootcampx queries 3 complete

--- 2_queries_joins/2_total_time_allstudents_cohort.sql
+++ 2_queries_joins/2_total_time_allstudents_cohort.sql
@@ -3,3 +3,11 @@ FROM assignment_submissions
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
+
+
+SELECT students.name AS student, COUNT(assignment_submissions) AS total_submissions
+FROM students
+JOIN assignment_submissions
+ON students.id = student_id
+GROUP BY students.name
+HAVING COUNT(assignment_submissions)<100;
